39104208991d4fa92c6ba9da6fbd708c63c73528
sarjus <sarju.s@sjcetpalai.ac.in>
2023-02-09 11:55:54 +0530

    Commit on 09-02-2023(1)

---
+++ Exercises/1.3.py
@@ -0,0 +1,11 @@
+"""
+Program: 1.3.py
+Author: Sarju S
+Last date modified: 09/02/23
+The purpose of this program is
+to compute the area of a triangle
+"""
+base = int(input("Enter the base value of the triangle: "))
+height = int(input("Enter the height value of the triangle:"))
+areaOfTriangle = 0.5*base*height
+print("Area of the given triangle= ",areaOfTriangle)
